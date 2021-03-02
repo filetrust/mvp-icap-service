0318c524c9d024989915124d7e84a13c61a1ad99
P Gerard <paulfgerard@gmail.com>
2021-03-02 16:37:59 +0000

    Refactor to pull out properties initialisation

--- cloud-proxy-app/test/AdaptationService/OutcomeHeaderFilterTests.cs
+++ cloud-proxy-app/test/AdaptationService/OutcomeHeaderFilterTests.cs
@@ -8,6 +8,14 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
 {
     public class OutcomeHeaderFilterTests
     {
+        Dictionary<string, object> properties;
+
+        [SetUp]
+        public void Init()
+        {
+            properties = new Dictionary<string, object>();
+        }
+
         [Test]
         public void Empty_Dictionary_If_No_Properties()
         {
@@ -15,7 +23,7 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
             var filter = new OutcomeHeaderFilter();
 
             // Act
-            var output = filter.Extract(new Dictionary<string, object>());
+            var output = filter.Extract(properties);
 
             // Assert
             Assert.That(output, Is.Empty, "Outcome Header filter should return an empty list of headers if the input is empty");
@@ -26,12 +34,8 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
         {
             // Arrange
             var filter = new OutcomeHeaderFilter();
-            var properties = new Dictionary<string, object>()
-            {
-                { "file-id", Guid.NewGuid() },
-                { "file-outcome", "replace"}
-            };
-
+            AddStandardProperties(properties);
+       
             // Act
             var output = filter.Extract(properties);
 
@@ -43,13 +47,9 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
         public void Single_Header_Is_Returned()
         {
             var filter = new OutcomeHeaderFilter();
-            var properties = new Dictionary<string, object>()
-            {
-                { "file-id", Guid.NewGuid() },
-                { "file-outcome", "replace"},
-                { "outcome-header-first-header", "First Header Value" }
-            };
-
+            AddStandardProperties(properties);
+            properties.Add("outcome-header-first-header", "First Header Value");
+   
             // Act
             var output = filter.Extract(properties);
 
@@ -58,6 +58,12 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
             Assert.That(output.Count, Is.EqualTo(1), "Outcome Header filter should return a single outcome-header entry");
             Assert.That(output, Contains.Key("first-header"), "Outcome Header filter extract the correct key");
             Assert.That(output["first-header"], Is.EqualTo("First Header Value"), "Outcome Header filter should extract the matching value");
+        }
+
+        private void AddStandardProperties(IDictionary<string, object> properties)
+        {
+            properties.Add("file-id", Guid.NewGuid());
+            properties.Add("file-outcome", "replace");
         }
     }
 }
