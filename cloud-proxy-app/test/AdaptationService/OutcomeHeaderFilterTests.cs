06da6913723eb576272651b4cabb680840f0c24b
P Gerard <paulfgerard@gmail.com>
2021-03-02 16:08:31 +0000

    Additional OutcomeHeaderFilter test to check the only related properties are processed

--- cloud-proxy-app/test/AdaptationService/OutcomeHeaderFilterTests.cs
+++ cloud-proxy-app/test/AdaptationService/OutcomeHeaderFilterTests.cs
@@ -1,6 +1,7 @@
 ﻿using Glasswall.IcapServer.CloudProxyApp.AdaptationService;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
@@ -8,7 +9,7 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
     public class OutcomeHeaderFilterTests
     {
         [Test]
-        public void Empty_Dictionary_If_No_Outcome_Headers()
+        public void Empty_Dictionary_If_No_Properties()
         {
             // Arrange
             var filter = new OutcomeHeaderFilter();
@@ -17,8 +18,25 @@ namespace Glasswall.IcapServer.CloudProxyApp.Tests.AdaptationService
             var output = filter.Extract(new Dictionary<string, object>());
 
             // Assert
-            Assert.That(output, Is.Empty, "Outcome HEader filter should return an empty list of headers if the input is empty");
+            Assert.That(output, Is.Empty, "Outcome Header filter should return an empty list of headers if the input is empty");
+        }
 
+        [Test]
+        public void Empty_Dictionary_If_No_Outcome_Headers()
+        {
+            // Arrange
+            var filter = new OutcomeHeaderFilter();
+            var properties = new Dictionary<string, object>()
+            {
+                { "file-id", Guid.NewGuid() },
+                { "file-outcome", "replace"}
+            };
+
+            // Act
+            var output = filter.Extract(properties);
+
+            // Assert
+            Assert.That(output, Is.Empty, "Outcome Header filter should return an empty list of headers if the input only has unrelated properties");
         }
     }
 }
