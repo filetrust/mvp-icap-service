12b35e66e45c7a1aa34ec00efc08397804b4d556
P Gerard <paulfgerard@gmail.com>
2021-03-03 14:33:20 +0000

    Addition of extension method to check for presence of return config file path

---
+++ cloud-proxy-app/source/Configuration/AppConfigurationExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Glasswall.IcapServer.CloudProxyApp.Configuration
+{
+    public static class AppConfigurationExtensions
+    {
+        public static bool ReturnConfigFilePathSpecified(this IAppConfiguration appConfiguration)
+        {
+            return !string.IsNullOrEmpty(appConfiguration.ReturnConfigFilepath);
+        }
+    }
+}
