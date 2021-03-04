939843632034002c467d8f938ffcfbc61933d264
P Gerard <paulfgerard@gmail.com>
2021-03-04 11:11:00 +0000

    Add test to verify that command line mapping key's match the data class to be bound.

---
+++ cloud-proxy-app/test/Configuration/CloudProxyApplicationConfigurationTests.cs
@@ -0,0 +1,40 @@
+﻿using Glasswall.IcapServer.CloudProxyApp.Configuration;
+using NUnit.Framework;
+
+namespace Glasswall.IcapServer.CloudProxyApp.Tests.Configuration
+{
+    class CloudProxyApplicationConfigurationTests
+    {
+        CloudProxyApplicationConfiguration configuration;
+
+        [SetUp]
+        public void SetUp()
+        {
+            configuration = new CloudProxyApplicationConfiguration();
+        }
+
+        [Test]
+        public void FileId_is_correctly_mapped_from_commandline()
+        {
+            Assert.That(nameof(configuration.FileId), Is.EqualTo(CommandLineSwitchMapping.FileIdConfigurationKey), "FileId Property and mapping key must be the same");
+        }
+
+        [Test]
+        public void InputFilepath_is_correctly_mapped_from_commandline()
+        {
+            Assert.That(nameof(configuration.InputFilepath), Is.EqualTo(CommandLineSwitchMapping.InputConfigurationKey), "InputFilepath Property and mapping key must be the same");
+        }
+
+        [Test]
+        public void OutputFilepath_is_correctly_mapped_from_commandline()
+        {
+            Assert.That(nameof(configuration.OutputFilepath), Is.EqualTo(CommandLineSwitchMapping.OutputConfigurationKey), "OutputFilepath Property and mapping key must be the same");
+        }
+
+        [Test]
+        public void ReturnConfigFilepath_is_correctly_mapped_from_commandline()
+        {
+            Assert.That(nameof(configuration.ReturnConfigFilepath), Is.EqualTo(CommandLineSwitchMapping.ReturnConfigurationKey), "ReturnConfigFilepath Property and mapping key must be the same");
+        }
+    }
+}
